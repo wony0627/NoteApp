9097e3c29b25ccf216dcaa61531ce9f973cf0bf4
wony0627 <119390816+wony0627@users.noreply.github.com>
2022-12-23 22:38:59 +0900

    Create EditScreen.js

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { TextInput, View, SafeAreaView, StyleSheet } from 'react-native'
+import { useRoute } from '@react-navigation/native'
+import { withContext } from 'react-simplified-context'
+import EditHeader from '../components/EditHeader'
+import { useNavigation } from '@react-navigation/native'
+
+const EditScreen = ({
+    create,
+    update,
+    articles,
+}) => {
+    const navigation = useNavigation()
+    const route = useRoute()
+    const id = route.params ? route.params.id : -1
+    const article = articles.find((a) => {
+        return a.id === id
+    })
+    let title = article ? article.title : ''
+    let content = article ? article.content : ''
+
+    return (
+    <SafeAreaView style={styles.container} >
+        <EditHeader done={() => {
+            if (id > -1) {
+                update(id, title, content)
+            } else {
+                create(title, content)
+            }
+        }} />
+        <View style={styles.body}>
+            <TextInput 
+            placeholder="제목" 
+            onChangeText={(text) => {
+                title = text
+            }}
+            style={styles.title}
+            >
+                {title}
+            </TextInput>
+            <View style={styles.divider} />
+            <TextInput 
+            placeholder="이곳을 눌러 작성을 시작하세요." 
+            multiline={true}
+            onChangeText={(text) => {
+                content = text
+            }}
+            style={styles.content}
+            >
+                {content}
+            </TextInput>
+        </View>
+    </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    body: {
+        flex: 1,
+        paddingTop: 8,
+        paddingBottom: 8,
+        paddingLeft: 16,
+        paddingRight: 16,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '600',
+        color: '#212121',
+    },
+    content: {
+        flex: 1,
+        fontSize: 16,
+        lineHeight: 20,
+        color: '#424242',
+    },
+    divider: {
+        marginTop: 12,
+        marginBottom: 12,
+        height: 1,
+        width: '100%',
+        backgroundColor: '#BDBDBD',
+    }
+})
+
+export default withContext(EditScreen)
